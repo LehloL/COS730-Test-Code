36d84238d8821b457af34b932d643da1815379b1
raphiritv <raphiritv@gmail.com>
2016-05-17 11:27:43 +0200

    Database Creation Scripts
    Database Creation Scripts

---
+++ SQL.sql
@@ -0,0 +1,23 @@
+﻿insert into person(id,firstname, surname)
+values(100, 'Hlengekile', 'Raphiri');
+
+
+
+insert into person(id,firstname, surname)
+values(200, 'John', 'Tleane');
+
+insert into person(id,firstname, surname)
+values(300, 'Victor', 'Raphiri');
+
+insert into person(id,firstname, surname)
+values(400, 'Tsebo', 'Raphiri');
+
+insert into person(id,firstname, surname)
+values(500, 'Joseph', 'Mokoena');
+
+insert into person(id,firstname, surname)
+values(600, 'Thuli', 'Madonsela');
+
+commit
+
+select * from person;
